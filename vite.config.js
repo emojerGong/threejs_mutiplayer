57aad328fdf8ec9125de8e2b720194d6b784ab65
EmojerGong <emojer@radiance.com.hk>
2023-09-19 15:07:07 +0800

    1. 添加本地colyseus server； 2. 更新啓動方式

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { join } from "path";
+import { join } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": join(__dirname, "src"),
+      '@': join(__dirname, 'src'),
+    },
+  },
+  server: {
+    // 这是默认值，它将把所有路径中含有 example 的文件
+    // 添加到忽略列表中。
+    sourcemapIgnoreList(sourcePath, sourcemapPath) {
+      return sourcePath.includes('example');
     },
   },
 });
